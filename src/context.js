809094b639f88c933498030f4f78ffe5a91057f7
Stefano <tepo@hotmail.it>
2023-04-05 21:21:53 +0200

    setting up global context

--- src/context.js
+++ src/context.js
@@ -2,19 +2,38 @@ import axios from 'axios'
 import React, { useState, useContext, useEffect } from 'react'
 
 const table = {
+  television: 14,
+  videoGames: 15,
+  boardGames: 16,
   sports: 21,
   history: 23,
   politics: 24,
-}
+};
 
-const API_ENDPOINT = 'https://opentdb.com/api.php?'
+const API_ENDPOINT = 'https://opentdb.com/api.php?';
 
-const url = ''
+const url = '';
+const tempUrl =
+  "https://opentdb.com/api.php?amount=10&category=16&difficulty=easy&type=multiple";
 
-const AppContext = React.createContext()
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  const [waiting, setWaiting] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const [error, setError] = useState(false);
+  const [modal, setModal] = useState(false);
+
+  return (
+    <AppContext.Provider
+      value={{ waiting, loading, questions, index, correct, error, modal }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 }
 // make sure use
 export const useGlobalContext = () => {
